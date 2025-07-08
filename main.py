469940241443723942ac677aaf0e1ec3429a1ddc
aman-deep940 <amandeep.vdoit@gmail.com>
2025-07-08 18:17:12 +0530

    added jwt

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
 
+from pydantic import BaseModel
+from .auth.authHandler import sign_jwt
+
 app = FastAPI()
 
+class SignUp(BaseModel):
+    user_id:str
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+
+@app.post("/signUp")
+async def signUp(response:SignUp):
+
+    print(response)
+    response = sign_jwt(user_id=response.user_id)
+    return response
+
+    
